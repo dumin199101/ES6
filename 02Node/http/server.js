c35b07ae57c23770d43211ab4418763140618233
dumin199101 <1766266374@qq.com>
2020-03-04 11:22:32 +0800

    node入门案例：通过http模块编写服务器

---
+++ 02Node/http/server.js
@@ -0,0 +1,12 @@
+// 创建一个http服务器
+var http = require("http");
+var server = http.createServer();
+server.listen(8080,function () {
+    console.log("127.0.0.1:8080");
+});
+
+server.on('request',function (res,rep) {
+    console.log(res.method);
+    rep.write("OK");
+    rep.end();
+});

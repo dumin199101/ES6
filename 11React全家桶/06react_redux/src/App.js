ad5ea31c8af49d72b1ee93db239e7348f617a254
dumin199101 <1766266374@qq.com>
2022-05-09 16:34:28 +0800

    React之Redux小应用-react-redux多组件共享数据

---
+++ 11React全家桶/06react_redux/src/App.js
@@ -0,0 +1,18 @@
+import {Component} from "react";
+import Count from "./containers/Count";
+import Person from "./containers/Person"
+
+
+export default class App extends Component{
+
+    render() {
+      return (
+          <div>
+              <Count/>
+              <hr/>
+              <Person/>
+          </div>
+      )
+    }
+
+}
